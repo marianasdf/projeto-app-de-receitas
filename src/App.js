2d96e4501a36544d8c62155f4440908c36ecf550
Juliana Braga <juliana4braga@gmail.com>
2021-11-09 17:40:24 -0300

    Correção

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Routes from './components/Routes';
 import RecipesProvider from './context/RecipesProvider';
@@ -11,13 +10,6 @@ function App() {
     <RecipesProvider>
       <div className="meals">
         <span className="logo">TRYBE</span>
-        <object
-          className="rocksGlass"
-          type="image/svg+xml"
-          data={ rockGlass }
-        >
-          Glass
-        </object>
         <BrowserRouter>
           <Routes />
         </BrowserRouter>
