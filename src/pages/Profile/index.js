1894f1ad0c26168e1094ee9bc41bdd0b53188ed5
Thales <thalessilva.tv@gmail.com>
2021-11-10 18:40:56 -0300

    tela de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../../components/Header';
 
-function Profile() {
+function Profile({ history }) {
+  const [localEmail, setLocalEmail] = useState('');
+
+  useEffect(() => setLocalEmail(JSON.parse(localStorage.getItem('user')).email), []);
+
   return (
-    <Header title="Perfil" />
+    <>
+      <Header title="Perfil" />
+      <h4 data-testid="profile-email">{localEmail}</h4>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => history.push('/receitas-feitas') }
+      >
+        Receitas Feitas
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => history.push('/receitas-favoritas') }
+      >
+        Receitas Favoritas
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ () => {
+          localStorage.clear();
+          history.push('/');
+        } }
+      >
+        Sair
+      </button>
+    </>
   );
 }
+
+Profile.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Profile;
