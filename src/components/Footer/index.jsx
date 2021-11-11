7f36fee3f75e715695e6ac69a6539ab4109743d2
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-11-11 15:35:00 -0400

    Implementa a tela de explorar comidas

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Redirect } from 'react-router';
-import drinkIcon from '../images/drinkIcon.svg';
-import exploreIcon from '../images/exploreIcon.svg';
-import foodIcon from '../images/mealIcon.svg';
+import drinkIcon from '../../images/drinkIcon.svg';
+import exploreIcon from '../../images/exploreIcon.svg';
+import foodIcon from '../../images/mealIcon.svg';
 
 export default function Footer() {
   function handleClick({ target: { name } }) {
