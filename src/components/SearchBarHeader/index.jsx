d92abf8c61e19ba05ef01d1ac893bcb36f945dc9
Juliana Braga <juliana4braga@gmail.com>
2021-11-11 10:13:20 -0300

    teste re-run

--- src/components/SearchBarHeader/index.jsx
+++ src/components/SearchBarHeader/index.jsx
@@ -13,6 +13,7 @@ function SearchBarHeader() {
     setSearchRadio(value);
   };
 
+  // teste
   const handleClick = async () => {
     const result = await fetchSearchBar(searchRadio, searchInput, pathUrl);
     if (result.meals === null || result.drinks === null) {
