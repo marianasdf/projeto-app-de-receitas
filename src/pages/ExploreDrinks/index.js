a5f0efb5da29d28069e698de1ed2f8ed8ea8a841
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-11-11 15:49:32 -0400

    Implementa a tela de explorar bebidas

--- src/pages/ExploreDrinks/index.js
+++ src/pages/ExploreDrinks/index.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 
-function ExploreDrinks() {
+function ExploreMeals() {
   return (
-    <Header title="Explorar Bebidas" />
+    <div>
+      <Header title="Explorar Comidas" />
+      <div>
+        <Link to="/explorar/bebidas/ingredientes">
+          <button
+            type="button"
+            data-testid="explore-by-ingredient"
+          >
+            Por Ingredientes
+          </button>
+        </Link>
+        <Link to="/explorar/bebidas/area">
+          <button
+            type="button"
+            data-testid="explore-by-area"
+          >
+            Por Local de Origem
+          </button>
+        </Link>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="explore-surprise"
+          >
+            Me Surpreenda!
+          </button>
+        </Link>
+      </div>
+      <Footer />
+    </div>
   );
 }
 
-export default ExploreDrinks;
+export default ExploreMeals;
